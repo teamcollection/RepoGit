d7da20e0e78d3836095416867d6d7540f080cdba
monica-tandon <48693264+monica-tandon@users.noreply.github.com>
2019-03-18 17:45:26 -0500

    Add files via upload

---
+++ ProjectLedger/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectLedger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The sum of two numbers 5 and 6 is: "+ Sum(5, 6));
+
+            System.Threading.Thread.Sleep(1000);
+
+
+        }
+
+        public static int Sum (int a, int b)
+        {
+            return a + b; 
+        }
+
+
+    }
+}
